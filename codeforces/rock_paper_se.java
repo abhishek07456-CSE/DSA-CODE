d5543a9d1053fa1082a96a08e92a7e75b54ec575
abhishek07456.cse <abhishek07456.cse@gmail.com>
2020-10-04 00:41:21 +0530

    changes

--- codeforces/rock_paper_se.java
+++ codeforces/rock_paper_se.java
@@ -1,14 +1,13 @@
 import java.util.Scanner;
+// rock  scissors paper
+// rock scissors  -> rock
+//scissors paper -> scissors
+//rock paper -> paper
 public class rock_paper_se {
     public static void main(String[] args) {
         Scanner read =new Scanner(System.in);
         int number_of_turns = read.nextInt();
-        int alice[] = new int[number_of_turns];
-        int bob[] = new int[number_of_turns];
-        int i = 0;
-        while(i!=n){
-            alic
-            number_of_turns--;
-        }
+        String alice = read.nextLine();
+        String bob = read.nextLine();
     }
 }
