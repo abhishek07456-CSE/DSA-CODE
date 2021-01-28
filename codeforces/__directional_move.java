6eaeb529c10c0129dc9c02ac0ecedad13409f126
abhishek07456.cse <abhishek07456.cse@gmail.com>
2021-01-28 19:26:37 +0530

    imp question

--- codeforces/__directional_move.java
+++ codeforces/__directional_move.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class directional_move {
+public class __directional_move {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
