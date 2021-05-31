2ec96bc8a246819574320780ccdefe7dd9354275
abhishek07456.cse <abhishek07456.cse@gmail.com>
2021-05-31 21:22:19 +0530

    fileoutstram

--- JAVA/FILE_HANDLING/FileOPStream.java
+++ JAVA/FILE_HANDLING/FileOPStream.java
@@ -8,11 +8,11 @@ import java.io.IOException;
 public class FileOPStream {
   public static void main(String[] args) {
       File file = new File("JAVA/FILE_HANDLING/file_output_stream.txt");
-      try( FileOutputStream fo = new FileOutputStream(file)){
+      try( FileOutputStream fo = new FileOutputStream(file,true)){
         if(!file.exists()) file.createNewFile();
-        String data = "hello \n we are introducing this feature";
+        String data = "hello we are introducing this feature\n";
         byte[] bs = data.getBytes();
-        fo.write(bs);
+        fo.write(bs,1,bs.length-1);
         System.out.println(bs.toString());
       }catch(IOException e){
          System.out.println(e.getMessage());
