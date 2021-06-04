c25308085f6039d2758c38466a282226db538615
abhishek07456.cse <abhishek07456.cse@gmail.com>
2021-06-04 22:06:31 +0530

    check directoy

---
+++ JAVA/FILE_HANDLING/Directory.java
@@ -0,0 +1,15 @@
+package JAVA.FILE_HANDLING;
+
+import java.io.File;
+
+public class Directory {
+    public static void main(String[] args) {
+        File file = new File("JAVA/FILE_HANDLING");
+        if(file.isDirectory()){
+            File list[] = file.listFiles();
+            for(File f : list){
+               System.out.println(f.getName());
+            }
+        }
+    }
+}
