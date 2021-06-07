9df50e7cdb33b243a2689bec0f9fbfe2a3e49b43
abhishek07456.cse <abhishek07456.cse@gmail.com>
2021-06-07 10:27:13 +0530

    ds revised

--- JAVA/FILE_HANDLING/Directory.java
+++ JAVA/FILE_HANDLING/Directory.java
@@ -5,6 +5,7 @@ import java.io.File;
 public class Directory {
     public static void main(String[] args) {
         File file = new File("JAVA/FILE_HANDLING");
+        FileUtils.
         if(file.isDirectory()){
             File list[] = file.listFiles();
             for(File f : list){
