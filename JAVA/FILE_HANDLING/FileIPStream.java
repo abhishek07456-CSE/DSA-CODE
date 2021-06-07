54cb720c99521936e7c3913c405829f47bae61dd
abhishek07456.cse <abhishek07456.cse@gmail.com>
2021-06-07 17:36:46 +0530

    file input starem

--- JAVA/FILE_HANDLING/FileIPStream.java
+++ JAVA/FILE_HANDLING/FileIPStream.java
@@ -8,11 +8,15 @@ import java.io.IOException;
 public class FileIPStream {
     public static void main(String[] args) {
         try{
-         FileInputStream file = new FileInputStream(new File("F:\\Abhishek Chaudhary.pdf"));
+         FileInputStream file = new FileInputStream(new File("JAVA/FILE_HANDLING/file_output_stream.txt"));
          int i ;
+         long start = System.nanoTime();
          while((i = file.read())!= -1){
            System.out.print((char)i);
          }
+         long end = System.nanoTime();
+         System.out.println(end-start);
+
         }catch(IOException E){
 
         }finally{
