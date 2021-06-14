6f8f13f43e6e187c666c85fa1bab0d5c1183be78
abhishek07456.cse <abhishek07456.cse@gmail.com>
2021-06-14 21:31:09 +0530

    update next greatr elemnt

--- Data_Structure/Stack/NextGreaterElement.java
+++ Data_Structure/Stack/NextGreaterElement.java
@@ -3,25 +3,25 @@ package Data_Structure.Stack;
 import java.util.Stack;
 
 public class NextGreaterElement {
-    public static long[] nextLargerElement(long[] arr, int n)
-    {
+    public static long[] nextLargerElement(long[] arr, int n) {
         // 7 8 1 2 4
-        Stack <Integer> s = new Stack<Integer>();
-        for(int i = 0 ; i < n ; i++){
-            while(!s.empty() && arr[s.peek()] < arr[i]){
-                arr[s.pop()] = arr[i];
+        Stack<Integer> s = new Stack<Integer>();
+        for (int i = 0; i < n; i++) {
+            while (!s.empty() && arr[s.peek()] < arr[i]) { //check current element is greater of top element
+                arr[s.pop()] = arr[i]; // update reaming index with this next greater element
             }
             s.push(i);
         }
-        while(!s.empty()){
-            arr[s.pop()] = -1;
+        while (!s.empty()) {
+            arr[s.pop()] = -1; // left element is stack have not found any next greater
         }
         return arr;
     }
+
     public static void main(String[] args) {
-        long [] arr = {7,8,1,3,4};
+        long[] arr = { 7, 8, 1, 3, 4 };
         nextLargerElement(arr, arr.length);
-        for(int i = 0 ; i < arr.length ; i++){
+        for (int i = 0; i < arr.length; i++) {
             System.out.println(arr[i]);
         }
     }
