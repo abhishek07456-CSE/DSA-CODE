c3fe0f57eb630c524dcb808676e4c2ebbb57e5e1
abhishek07456.cse <abhishek07456.cse@gmail.com>
2021-06-29 16:57:50 +0530

    longest substring

--- Data_Structure/String/LongestSubstringNonRepeating.java
+++ Data_Structure/String/LongestSubstringNonRepeating.java
@@ -14,13 +14,14 @@ public class LongestSubstringNonRepeating {
               map.put(s.charAt(i),i);
             }else{
              //ignore if after staring point eg abba st point is 2 when a repeated so we can add a current substring
+             //current element index must be greater then starting point eg a is repeated at last index but at this point st point from b ie. 2 index and last index of a is 1 , so we can add a into current substring
              if(map.get(s.charAt(i)) >= st){
                   l =  i - st;
                   if(l > max){
                       max = l;
                       start = st;
                   }
-                  st = map.get(s.charAt(i)) + 1; //st point after  repeated string eg dvfd so vdf will next string for check
+                  st = map.get(s.charAt(i)) + 1; //st point after  repeated string eg [dvfd] so [vdf] will next string for check because d  is repated  so next substring is last string + 1 postition
              }
              map.replace(s.charAt(i), i);
 
