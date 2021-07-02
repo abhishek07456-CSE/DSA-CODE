c74e58b36c6ae0d41c9f5630d42664a78f1a5381
abhishek07456.cse <abhishek07456.cse@gmail.com>
2021-07-02 14:04:25 +0530

    es6

--- JS/es/es.js
+++ JS/es/es.js
@@ -1,6 +1,40 @@
 //finite check wether is finit or not
+{
 let a = 1;
 let b = 0;
 if(isFinite(a/b))
  console.log("yes");
+}
 
+{
+    /*
+     -for of iterate over value
+     -for in iterate over key
+
+    */
+let a = "hee;;o";
+  for(let char of a){
+      console.log(char);
+  }
+  console.log("for of -----")
+  for(let char of a){
+    console.log(char);
+  }
+  console.log("for in -----")
+  for(let char in a){
+    console.log(char);
+  }
+
+  let obj = {"name" : "abhishek" , age : 18};
+  console.log(obj);
+  console.log("for of -----")
+  //object.keys
+  //object.values
+  //object.entries
+  for(let char of Object.entries(obj)){
+     const [key , value] = char;
+    console.log(value);
+  }
+
+
+}
